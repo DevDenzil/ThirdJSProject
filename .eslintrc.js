7cc6aa8a37a61ea9a7f563212cfb11a681e2a0fa
Denys <denzil92@gmail.com>
2023-06-07 16:48:22 +0200

    Timer

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: 'eslint:recommended',
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    indent: ['error', 2],
+    'linebreak-style': ['error', 'windows'],
+    quotes: ['warn', 'single'],
+    semi: ['error', 'always'],
+    'no-unused-vars': ['warn'],
+    'no-debugger': 'off',
+  },
+};
--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 'use strict';
-
+// TABS
 window.addEventListener('DOMContentLoaded', () => {
   const tabs = document.querySelectorAll('.tabheader__item');
   const tabsContent = document.querySelectorAll('.tabcontent');
@@ -41,4 +41,87 @@ window.addEventListener('DOMContentLoaded', () => {
       });
     }
   });
+
+  //Timer
+  const monthes = [
+    'Января',
+    'Февраля',
+    'Марта',
+    'Апреля',
+    'Мая',
+    'Июня',
+    'Июля',
+    'Августа',
+    'Сентября',
+    'Октября',
+    'Ноября',
+    'Декабря',
+  ];
+  const actionEnd = document.querySelector('.promotion__descr');
+  const deadline = '2023-06-08';
+  const dateDeadline = new Date(deadline);
+
+  actionEnd.innerHTML = actionEnd.innerHTML.replace(
+    'Акция закончится 20 мая в 00:00',
+    `Акция закончится ${dateDeadline.getDate()} ${
+      monthes[dateDeadline.getMonth()]
+    } в 00:00`
+  );
+
+  function getTimeRemaining(endtime) {
+    const t = Date.parse(endtime) - Date.parse(new Date());
+    let days = Math.floor(t / (1000 * 60 * 60 * 24));
+    let hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+    let minutes = Math.floor((t / (1000 * 60)) % 60);
+    let seconds = Math.floor((t / 1000) % 60);
+
+    if (t <= 0) {
+      days = '00';
+      hours = '00';
+      minutes = '00';
+      seconds = '00';
+    }
+
+    return {
+      total: t,
+      days: days,
+      hours: hours,
+      minutes: minutes,
+      seconds: seconds,
+    };
+  }
+
+  function getZero(num) {
+    if (num > 0 && num < 10) {
+      return `0${num}`;
+    } else {
+      return num;
+    }
+  }
+
+  function setClock(selector, endtime) {
+    const timer = document.querySelector(selector);
+    const days = timer.querySelector('#days');
+    const hours = timer.querySelector('#hours');
+    const minutes = timer.querySelector('#minutes');
+    const seconds = timer.querySelector('#seconds');
+    const timeInterval = setInterval(updateClock, 1000);
+
+    updateClock();
+
+    function updateClock() {
+      const t = getTimeRemaining(endtime);
+
+      days.innerHTML = getZero(t.days);
+      hours.innerHTML = getZero(t.hours);
+      minutes.innerHTML = getZero(t.minutes);
+      seconds.innerHTML = getZero(t.seconds);
+
+      if (t.total <= 0) {
+        clearInterval(timeInterval);
+      }
+    }
+  }
+
+  setClock('.timer', deadline);
 });
